0268646fa407a9f03907741c329831eda7218885
Tobias Liaudat <tobiasliaudat@gmail.com>
2024-02-09 17:53:19 +0000

    remove qos def

--- jean_zay/raw/diffnest/jobs/langevin_nest_wav_run1.sh
+++ jean_zay/raw/diffnest/jobs/langevin_nest_wav_run1.sh
@@ -15,7 +15,7 @@
 #SBATCH --error=R-%x_%A_%a.err   # nom du fichier d'erreur (ici commun avec la sortie)
 #SBATCH -A ynx@v100                   # specify the project
 ##SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
-#SBATCH --qos=qos_gpu-t4              # Long queue
+##SBATCH --qos=qos_gpu-t4              # Long queue
 #SBATCH --array=0-6
 
 cd $WORK/projects/submission-scripts/jean_zay/env_configs/

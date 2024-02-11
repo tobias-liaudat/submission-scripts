ec378fcf5f632b1cb8b4004312cf39efa5d9b7af
Tobias Liaudat <tobiasliaudat@gmail.com>
2024-02-11 16:22:16 +0000

    modif debug

--- jean_zay/wandb/debug_syncall_wandb.sh
+++ jean_zay/wandb/debug_syncall_wandb.sh
@@ -6,11 +6,10 @@ module load pytorch-gpu/py3/2.1.1
 
 offline_runs="$1/offline-run*"
 
-while :
-do
+
 for ofrun in $offline_runs
 do
         echo $ofrun;
+        sleep 1s;
 done
-    sleep 1s
-done
+

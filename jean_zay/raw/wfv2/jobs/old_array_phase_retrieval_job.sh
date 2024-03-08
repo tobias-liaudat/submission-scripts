662bc8d0877b54e13348f65d351e4e79360e3c93
Tobias Liaudat <tobiasliaudat@gmail.com>
2024-03-08 16:58:48 +0100

    add old array job

---
+++ jean_zay/raw/wfv2/jobs/old_array_phase_retrieval_job.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=PR_val_wfv2_test_n2_    # nom du job
+##SBATCH --partition=gpu_p2          # de-commente pour la partition gpu_p2
+#SBATCH --ntasks=1                   # nombre total de tache MPI (= nombre total de GPU)
+#SBATCH --ntasks-per-node=1          # nombre de tache MPI par noeud (= nombre de GPU par noeud)
+#SBATCH --gres=gpu:1                 # nombre de GPU par n?~Sud (max 8 avec gpu_p2)
+#SBATCH --cpus-per-task=10           # nombre de coeurs CPU par tache (un quart du noeud ici)
+#SBATCH -C v100-32g 
+# /!\ Attention, "multithread" fait reference a l'hyperthreading dans la terminologie Slurm
+#SBATCH --hint=nomultithread         # hyperthreading desactive
+#SBATCH --time=20:00:00              # temps d'execution maximum demande (HH:MM:SS)
+#SBATCH --output=PR_val_wfv2_test_n2_%j.out  # nom du fichier de sortie
+#SBATCH --error=PR_val_wfv2_test_n2_%j.err   # nom du fichier d'erreur (ici commun avec la sortie)
+#SBATCH --mail-use=tobiasliaudat@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH -A ynx@v100                   # specify the project
+##SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
+#SBATCH --array=0-3
+
+# Load env
+cd $WORK/projects/submission-scripts/jean_zay/env_configs/
+. wfv2.sh
+
+
+# echo des commandes lancees
+set -x
+
+opt[0]=" -c /gpfswork/rech/ynx/ulx23va/projects/submission-scripts/jean_zay/raw/wfv2/configs/phase-retrieval-PR/PR_configs_v0.yaml -r /gpfswork/rech/ynx/ulx23va/wfv2/repos/phase-retrieval-PR/wf-psf -o /gpfsscratch/rech/ynx/ulx23va/wfv2/phase-retrieval-PR/output_v2"
+opt[1]=" -c /gpfswork/rech/ynx/ulx23va/projects/submission-scripts/jean_zay/raw/wfv2/configs/phase-retrieval-PR/PR_configs_v1.yaml -r /gpfswork/rech/ynx/ulx23va/wfv2/repos/phase-retrieval-PR/wf-psf -o /gpfsscratch/rech/ynx/ulx23va/wfv2/phase-retrieval-PR/output_v2"
+opt[2]=" -c /gpfswork/rech/ynx/ulx23va/projects/submission-scripts/jean_zay/raw/wfv2/configs/phase-retrieval-PR/PR_configs_v2.yaml -r /gpfswork/rech/ynx/ulx23va/wfv2/repos/phase-retrieval-PR/wf-psf -o /gpfsscratch/rech/ynx/ulx23va/wfv2/phase-retrieval-PR/output_v2"
+opt[3]=" -c /gpfswork/rech/ynx/ulx23va/projects/submission-scripts/jean_zay/raw/wfv2/configs/phase-retrieval-PR/PR_configs_v3.yaml -r /gpfswork/rech/ynx/ulx23va/wfv2/repos/phase-retrieval-PR/wf-psf -o /gpfsscratch/rech/ynx/ulx23va/wfv2/phase-retrieval-PR/output_v2"
+
+
+wavediff ${opt[$SLURM_ARRAY_TASK_ID]}
+

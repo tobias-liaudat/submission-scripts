4170169faf33ef69a8cf8c1a40a56cdc4b5f3b0b
Tobias Liaudat <tobiasliaudat@gmail.com>
2024-07-31 19:57:52 +0200

    add mydenoisers jobs

---
+++ jean_zay/raw/mydenoisers/jobs/BSD_unet_training.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=BSD_unet_training    # nom du job
+#SBATCH --mail-user=tobiasliaudat@gmail.com
+#SBATCH --mail-type=ALL
+##SBATCH --partition=gpu_p2          # de-commente pour la partition gpu_p2
+#SBATCH --ntasks=1                   # nombre total de tache MPI (= nombre total de GPU)
+#SBATCH --ntasks-per-node=1          # nombre de tache MPI par noeud (= nombre de GPU par noeud)
+#SBATCH --gres=gpu:1                 # nombre de GPU par n?~Sud (max 8 avec gpu_p2)
+#SBATCH --cpus-per-task=10           # nombre de coeurs CPU par tache (un quart du noeud ici)
+#SBATCH -C v100-32g 
+# /!\ Attention, "multithread" fait reference a l'hyperthreading dans la terminologie Slurm
+#SBATCH --hint=nomultithread         # hyperthreading desactive
+#SBATCH --time=20:00:00              # temps d'execution maximum demande (HH:MM:SS)
+#SBATCH --output=R-%x_%A_%a.out  # nom du fichier de sortie
+#SBATCH --error=R-%x_%A_%a.err   # nom du fichier d'erreur (ici commun avec la sortie)
+#SBATCH -A ynx@v100                   # specify the project
+##SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
+##SBATCH --qos=qos_gpu-t4              # Long queue
+##SBATCH --array=0-6
+
+cd $WORK/projects/submission-scripts/jean_zay/
+
+. ./env_configs/mydenoisers.sh
+
+# echo des commandes lancees
+set -x
+
+srun python -u $WORK/projects/projects-ns/myDenoisers/scripts/runner_train_denoiser.py \
+    -c ./raw/mydenoisers/configs/BSD_unet_training.yml -s
+

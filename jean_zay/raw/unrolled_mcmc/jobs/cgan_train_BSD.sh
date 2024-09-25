53881aae9022407537d3d6ca8c8b9227941592f8
Tobias Liaudat <tobiasliaudat@gmail.com>
2024-09-25 23:41:47 +0100

    add new job and configs

---
+++ jean_zay/raw/unrolled_mcmc/jobs/cgan_train_BSD.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=cgan_train_GaussDeblur_BSD    # nom du job
+#SBATCH --mail-user=tobiasliaudat@gmail.com
+#SBATCH --mail-type=ALL
+##SBATCH --partition=gpu_p2          # de-commente pour la partition gpu_p2
+#SBATCH --ntasks=1                   # nombre total de tache MPI (= nombre total de GPU)
+#SBATCH --ntasks-per-node=1          # nombre de tache MPI par noeud (= nombre de GPU par noeud)
+#SBATCH --gres=gpu:1                 # nombre de GPU par n?~Sud (max 8 avec gpu_p2)
+#SBATCH --cpus-per-task=10           # nombre de coeurs CPU par tache (un quart du noeud ici)
+#SBATCH -C v100-32g 
+# /!\ Attention, "multithread" fait reference a l'hyperthreading dans la terminologie Slurm
+#SBATCH --hint=nomultithread         # hyperthreading desactive
+#SBATCH --time=99:59:00              # temps d'execution maximum demande (HH:MM:SS)
+#SBATCH --output=R-%x_%A_%a.out  # nom du fichier de sortie
+#SBATCH --error=R-%x_%A_%a.err   # nom du fichier d'erreur (ici commun avec la sortie)
+#SBATCH -A rbn@v100                   # specify the project
+##SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
+#SBATCH --qos=qos_gpu-t4              # Long queue
+##SBATCH --array=0-6
+
+# echo des commandes lancees
+set -x
+
+cd /lustre/fswork/projects/rech/rbn/ulx23va/projects/unrolled_cGAN/repos/unrolled_cgan/
+
+bash /lustre/fswork/projects/rech/rbn/ulx23va/projects/unrolled_cGAN/repos/submission-scripts/jean_zay/env_configs/unrolled_mcmc.sh
+
+
+srun python -u unrolled_cgan.py \
+    --config /lustre/fswork/projects/rech/rbn/ulx23va/projects/unrolled_cGAN/repos/submission-scripts/jean_zay/raw/unrolled_mcmc/configs/args_BSD.yml
+

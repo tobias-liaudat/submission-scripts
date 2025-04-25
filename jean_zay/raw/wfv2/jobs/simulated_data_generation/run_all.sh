aadcc486bfb1daa9905ddde9ef1d7937c32844e2
Tobias Liaudat <tobiasliaudat@gmail.com>
2025-04-25 16:43:19 +0200

    update path

--- jean_zay/raw/wfv2/jobs/simulated_data_generation/run_all.sh
+++ jean_zay/raw/wfv2/jobs/simulated_data_generation/run_all.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-sbatch data_generation_params_v.0.1.0.sh
-sbatch data_generation_params_v.0.2.0.sh
-sbatch data_generation_params_v.0.3.0.sh
+sbatch ./../data_generation_params_v.0.1.0.sh
+sbatch ./../data_generation_params_v.0.2.0.sh
+sbatch ./../data_generation_params_v.0.3.0.sh
 
 
-sbatch data_generation_params_v.1.1.0.sh
-sbatch data_generation_params_v.1.2.0.sh
-sbatch data_generation_params_v.1.3.0.sh
+sbatch ./../data_generation_params_v.1.1.0.sh
+sbatch ./../data_generation_params_v.1.2.0.sh
+sbatch ./../data_generation_params_v.1.3.0.sh
 
 
-sbatch data_generation_params_v.2.1.0.sh
-sbatch data_generation_params_v.2.2.0.sh
-sbatch data_generation_params_v.2.3.0.sh
+sbatch ./../data_generation_params_v.2.1.0.sh
+sbatch ./../data_generation_params_v.2.2.0.sh
+sbatch ./../data_generation_params_v.2.3.0.sh
 
 
-sbatch data_generation_params_v.3.1.0.sh
-sbatch data_generation_params_v.3.2.0.sh
-sbatch data_generation_params_v.3.3.0.sh
+sbatch ./../data_generation_params_v.3.1.0.sh
+sbatch ./../data_generation_params_v.3.2.0.sh
+sbatch ./../data_generation_params_v.3.3.0.sh
 

1c342b613d26e2cba605f57e1f9540e48decc15c
Tobias Liaudat <tobiasliaudat@gmail.com>
2025-04-25 17:06:27 +0200

    update to rerun failed scripts

--- jean_zay/raw/wfv2/jobs/simulated_data_generation/run_all.sh
+++ jean_zay/raw/wfv2/jobs/simulated_data_generation/run_all.sh
@@ -10,12 +10,12 @@ sbatch ./../data_generation_params_v.1.2.0.sh
 sbatch ./../data_generation_params_v.1.3.0.sh
 
 
-sbatch ./../data_generation_params_v.2.1.0.sh
-sbatch ./../data_generation_params_v.2.2.0.sh
-sbatch ./../data_generation_params_v.2.3.0.sh
+# sbatch ./../data_generation_params_v.2.1.0.sh
+# sbatch ./../data_generation_params_v.2.2.0.sh
+# sbatch ./../data_generation_params_v.2.3.0.sh
 
 
-sbatch ./../data_generation_params_v.3.1.0.sh
-sbatch ./../data_generation_params_v.3.2.0.sh
-sbatch ./../data_generation_params_v.3.3.0.sh
+# sbatch ./../data_generation_params_v.3.1.0.sh
+# sbatch ./../data_generation_params_v.3.2.0.sh
+# sbatch ./../data_generation_params_v.3.3.0.sh
 

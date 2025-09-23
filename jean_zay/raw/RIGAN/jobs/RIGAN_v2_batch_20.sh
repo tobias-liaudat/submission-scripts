a1b53be7736a981d046ed1c72e774088b0b99d1f
Tobias Liaudat <tobiasliaudat@gmail.com>
2025-09-23 17:53:56 +0200

    set to 1 gpu

--- jean_zay/raw/RIGAN/jobs/RIGAN_v2_batch_20.sh
+++ jean_zay/raw/RIGAN/jobs/RIGAN_v2_batch_20.sh
@@ -5,7 +5,7 @@
 ##SBATCH --partition=gpu_p2          # de-commente pour la partition gpu_p2
 #SBATCH --ntasks=1                   # nombre total de tache MPI (= nombre total de GPU)
 #SBATCH --ntasks-per-node=1          # nombre de tache MPI par noeud (= nombre de GPU par noeud)
-#SBATCH --gres=gpu:2                 # nombre de GPU par n?~Sud (max 8 avec gpu_p2)
+#SBATCH --gres=gpu:1                 # nombre de GPU par n?~Sud (max 8 avec gpu_p2)
 #SBATCH --cpus-per-task=10           # nombre de coeurs CPU par tache (un quart du noeud ici)
 #SBATCH -C v100-32g 
 # /!\ Attention, "multithread" fait reference a l'hyperthreading dans la terminologie Slurm
@@ -33,5 +33,5 @@ export TMPDIR=$JOBSCRATCH
 srun python -u train.py \
     --config /lustre/fswork/projects/rech/rbn/ulx23va/projects/unrolled_cGAN/repos/submission-scripts/jean_zay/raw/RIGAN/configs/radio_meerkat_macro-v2_batch_20.yml \
     --exp-name RIGAN_v2_batch_20 \
-    --num-gpus 2
+    --num-gpus 1
 
